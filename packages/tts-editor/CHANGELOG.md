e638812f5d7bd6e247439ff8c58a498aa5429776
Sebastian Stern <sebastian.stern.42@gmail.com>
2025-02-02 14:30:04 +0100

    Bump VS Code version

--- packages/tts-editor/CHANGELOG.md
+++ packages/tts-editor/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Breaking
 
 - Removed (undocumented) support for TSTL
+- Minimum VS Code version bumped to 1.96.0
 
 ### Added
 
--- packages/tts-editor/eslint.config.mjs
+++ packages/tts-editor/eslint.config.mjs
@@ -3,4 +3,21 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+});
